92213a39bcd9db9a84d38d3f618c7ea96164d909
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 18:01:01 +0700

    correct code

--- io_03_write_binary.py
+++ io_03_write_binary.py
@@ -1,6 +1,5 @@
 with open('binary_01', 'bw') as encode:
-    for i in range(17):
-        encode.write(bytes([i]))
+    encode.write(bytes(range(17)))
 
 with open('binary_01', 'br') as read_code:
     for i in read_code:
