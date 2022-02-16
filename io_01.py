a1ff0661416d0079e1bd028ef347faa7dbd06481
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 14:53:44 +0700

    use .readline() method

--- io_01.py
+++ io_01.py
@@ -10,7 +10,13 @@
 # summary.close()
 
 # with also handle error on objects, and close object before error terminates program
-with open("set_summary.txt", "r") as source:
-    for line in source:                     # with method close object automaticly
-        print(line, end='')                 # once it's not needed
+# with open("set_summary.txt", "r") as source:
+#     for line in source:                     # with method close object automatically
+#         print(line, end='')                 # once it's not needed
+#
 
+with open("set_summary.txt", "r") as source:
+    line = source.readline()                # if file is empty it won't proceed to while loop
+    while line:
+        print(line, end='')
+        line = source.readline()
