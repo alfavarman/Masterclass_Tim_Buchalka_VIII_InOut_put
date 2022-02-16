e0b1d3eaffcd6306c6edf1334cdaff818ba7ac3c
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 16:57:33 +0700

    read objects from txt file with eval function and unpack it from variable

--- io_02_write.py
+++ io_02_write.py
@@ -3,9 +3,19 @@
 #     print(line)
 #     with open("new_file.txt", "w") as output:
 #         print(line, end='', file=output)
+#
+# object_1 = "this is insert as a text", "this is insert as string, as well ", (
+#     (0, "cannon"), (1, "bullet"), (4, "live"))
+#
+# with open('io_02_easy_to_write_hard_to_read.txt', 'w') as example:
+#     print(object_1, end='', file=example)
 
-object_1 = "this is insert as a text", "this is insert as string, as well ", (
-    (0, "cannon"), (1, "bullet"), (4, "live"))
+with open('io_02_easy_to_write_hard_to_read.txt', 'r') as content:
+    read_content = content.readline()
 
-with open('io_02_easy_to_write_hard_to_read.txt', 'w') as example:
-    print(object_1, end='', file=example)
+variable_for_objects = eval(read_content)
+print(variable_for_objects)
+a, b, c = variable_for_objects
+print(a)
+print(b)
+print(c)
