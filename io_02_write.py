75b6d0640a02b5af2f2bb27df6245ffb0db133f7
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 16:41:22 +0700

    open a source file and write it to a new file

---
+++ io_02_write.py
@@ -0,0 +1,5 @@
+# with open("set_summary.txt", "r") as source:
+#     line = source.readlines()
+#     print(line)
+#     with open("new_file.txt", "w") as output:
+#         print(line, end='', file=output)
