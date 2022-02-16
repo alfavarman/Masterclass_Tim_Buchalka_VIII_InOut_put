2fb4a54c31ca9a9b0232df83a7513f503e10d2a4
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 14:31:08 +0700

    first method to open a file, Python read file as a list

---
+++ io_01.py
@@ -0,0 +1,10 @@
+# summary = open("set_summary.txt", "r")
+#
+# # for line in summary:
+# #     print(line)
+#
+# for line in summary:
+#     if "set" in line.lower():
+#         print(line, end="")
+#
+# summary.close()
