50f0dd2f6cddf034089233bfed02d9e5ecc22586
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 16:48:49 +0700

    write object to a txt file

--- io_02_write.py
+++ io_02_write.py
@@ -3,3 +3,9 @@
 #     print(line)
 #     with open("new_file.txt", "w") as output:
 #         print(line, end='', file=output)
+
+object_1 = "this is insert as a text", "this is insert as string, as well ", (
+    (0, "cannon"), (1, "bullet"), (4, "live"))
+
+with open('io_02_easy_to_write_hard_to_read.txt', 'w') as example:
+    print(object_1, end='', file=example)
