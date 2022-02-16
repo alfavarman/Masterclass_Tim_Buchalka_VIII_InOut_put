832da226413b5a051e879e40e5b42fcebb790f86
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-16 15:03:48 +0700

    use .readlines() method

--- io_01.py
+++ io_01.py
@@ -14,9 +14,17 @@
 #     for line in source:                     # with method close object automatically
 #         print(line, end='')                 # once it's not needed
 #
+#
+# with open("set_summary.txt", "r") as source:
+#     line = source.readline()                # if file is empty it won't proceed to while loop
+#     while line:
+#         print(line, end='')
+#         line = source.readline()
+
 
 with open("set_summary.txt", "r") as source:
-    line = source.readline()                # if file is empty it won't proceed to while loop
-    while line:
-        print(line, end='')
-        line = source.readline()
+    lines = source.readlines()                # read all the lines, as a list
+print(lines, end='')                          # shows in each element \n which shows why
+                                              # use end=''
+for line in lines:
+    print(line, end='')
