65b1ebe7ee37b9e839ca574c0da336d0197a30cc
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-17 14:06:38 +0700

    2nd solution writeback=True ease of coding cost high memory usage.

--- io_05_shelves_modifing.py
+++ io_05_shelves_modifing.py
@@ -7,7 +7,7 @@ import shelve
 # khao_kluk_khapi = ['shrimp paste rice', 'veggies: mango, cucumber', 'sliced omelette', 'fried garlic']
 # khao_mu_krop = ['rice', 'crispy pork']
 
-with shelve.open('thai_dishes') as thai_menu:
+with shelve.open('thai_dishes', writeback=True) as thai_menu:
     # thai_menu['gra_pao_gai'] = gra_pao_gai
     # thai_menu['khao_kan_chin'] = khao_kan_chin
     # thai_menu['khao_kha_mu'] = khao_kha_mu
@@ -18,7 +18,6 @@ with shelve.open('thai_dishes') as thai_menu:
     print('=' * 60)
     for dish in thai_menu:
         print(dish, thai_menu[dish])
-
 
     # below doesn't work: chilli dip is appended to the object in memory
     # while for loop print object from shelve
@@ -31,6 +30,11 @@ with shelve.open('thai_dishes') as thai_menu:
     # temp_menu = thai_menu["khao_mu_krop"]
     # temp_menu.append("chilli dip")
     # thai_menu['khao_mu_krop'] = temp_menu
+
+    # SOLUTION 2: writeback = True line 10
+    # easier code, but havier memory usage
+    # python save modification when close with statement
+    thai_menu['tom_yum_khug'] = ['coconut milk', 'tom yum spices', 'chilli', 'shrimps']
 
     print('=' * 60)
     for dish in thai_menu:
