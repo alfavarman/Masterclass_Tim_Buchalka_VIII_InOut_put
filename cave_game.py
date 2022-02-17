bb91d05402a8075203b39dde008f66ab0b20feb7
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-17 15:35:54 +0700

    location refractor locations

--- cave_game.py
+++ cave_game.py
@@ -1,3 +1,5 @@
+import shelve
+
 # cave_game.py will then use the two shelves instead of dictionaries.
 # Apart from opening and closing the shelves, cave_game will need only
 # two changes to the actual code - remember that shelf keys MUST be strings!
@@ -5,30 +7,33 @@
 # Just to be clear, cave_game.py will contain the code from line 45, everything
 # before that (modified to use shelves) will be in cave_initialise.py.
 
-loc = 1
-while True:
-    availableExits = ", ".join(exits[loc].keys())
+with shelve.open('locations') as locations:
+    with shelve.open('vocabulary') as vocabulary:
 
-    print(locations[loc])
+    loc = 1
+    while True:
+        availableExits = ", ".join(exits[loc].keys())
 
-    if loc == 0:
-        break
-    else:
-        allExits = exits[loc].copy()
-        allExits.update(namedExits[loc])
+        print(locations[loc])
 
-    direction = input("Available exits are " + availableExits).upper()
-    print()
+        if loc == 0:
+            break
+        else:
+            allExits = exits[loc].copy()
+            allExits.update(namedExits[loc])
 
-    # Parse the user input, using our vocabulary dictionary if necessary
-    if len(direction) > 1:  # more than 1 letter, so check vocab
-        words = direction.split()
-        for word in words:
-            if word in vocabulary:  # does it contain a word we know?
-                direction = vocabulary[word]
-                break
+        direction = input("Available exits are " + availableExits).upper()
+        print()
 
-    if direction in allExits:
-        loc = allExits[direction]
-    else:
-        print("You cannot go in that direction")
+        # Parse the user input, using our vocabulary dictionary if necessary
+        if len(direction) > 1:  # more than 1 letter, so check vocab
+            words = direction.split()
+            for word in words:
+                if word in vocabulary:  # does it contain a word we know?
+                    direction = vocabulary[word]
+                    break
+
+        if direction in allExits:
+            loc = allExits[direction]
+        else:
+            print("You cannot go in that direction")
--- cave_initialise.py
+++ cave_initialise.py
@@ -6,19 +6,19 @@ with shelve.open('locations') as locations:
     locations['0'] = {"desc": "You are sitting in front of a computer learning Python",
                     "exits": {},
                     "namedExits": {}}
-    location['1'] = {"desc": "You are standing at the end of a road before a small brick building",
+    locations['1'] = {"desc": "You are standing at the end of a road before a small brick building",
                     "exits": {"W": 2, "E": 3, "N": 5, "S": 4, "Q": 0},
                     "namedExits": {"2": 2, "3": 3, "5": 5, "4": 4}}
-    location['2'] = {"desc": "You are at the top of a hill",
+    locations['2'] = {"desc": "You are at the top of a hill",
                     "exits": {"N": 5, "Q": 0},
                     "namedExits": {"5": 5}}
-    location['3'] = {"desc": "You are inside a building, a well house for a small stream",
+    locations['3'] = {"desc": "You are inside a building, a well house for a small stream",
                     "exits": {"W": 1, "Q": 0},
                     "namedExits": {"1": 1}}
-    location['4'] = {"desc": "You are in a valley beside a stream",
+    locations['4'] = {"desc": "You are in a valley beside a stream",
                     "exits": {"N": 1, "W": 2, "Q": 0},
                     "namedExits": {"1": 1, "2": 2}}
-    location['5'] = {"desc": "You are in the forest",
+    locations['5'] = {"desc": "You are in the forest",
                     "exits": {"W": 2, "S": 1, "Q": 0},
                     "namedExits": {"2": 2, "1": 1}}
 
